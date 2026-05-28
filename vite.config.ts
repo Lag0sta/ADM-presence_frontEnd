954f9ae12fb42e1826da5bdd34499443e690eb28
LRonnet <leo.ronnet@gmail.com>
2026-05-28 13:59:11 +0200

    test

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from '@tailwindcss/vite'
+
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'ADM Presence',
+        short_name: 'ADMp',
+        description: 'Gestion des présences',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    })],
+})
