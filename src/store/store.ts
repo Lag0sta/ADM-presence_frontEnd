cc2d05108e0a4a569b4ad12fcd273b5fbf175c66
LRonnet <leo.ronnet@gmail.com>
2026-06-12 13:24:10 +0200

    retourRedux Vercel

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,31 +1,14 @@
-import { configureStore, combineReducers  } from "@reduxjs/toolkit";
-import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from "redux-persist";
- 
+import { configureStore } from "@reduxjs/toolkit";
+
 import auth from "./reducers/auth.js";
 import student from "./reducers/student.js";
 import attendance from "./reducers/attendance.js";
 
 // Typage du store
-const rootReducer = combineReducers({
-  auth,
-  student,
-  attendance,
-});
-
-const persistConfig = {
-  key: "root",
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {attendance, auth, student},
 });
 
-export const persistor = persistStore(store);
-
-// Types
+// Définition du type RootState
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
