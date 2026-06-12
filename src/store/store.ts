a98247f2e44ff8077c63b433abf8bd4d13e284a9
LRonnet <leo.ronnet@gmail.com>
2026-06-12 12:46:45 +0200

    corrected store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
-
+import { configureStore, combineReducers  } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+ 
 import auth from "./reducers/auth.js";
 import student from "./reducers/student.js";
 import attendance from "./reducers/attendance.js";
 
 // Typage du store
-export const store = configureStore({
-  reducer: {attendance, auth, student},
+const rootReducer = combineReducers({
+  auth,
+  student,
+  attendance,
 });
 
-// Définition du type RootState
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+// Types
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
