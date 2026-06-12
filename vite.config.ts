4f824215c66517ec6ef22c5118f8d2be5395be32
LRonnet <leo.ronnet@gmail.com>
2026-06-12 15:09:59 +0200

    correct Icon

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: './public/BanderoleFuzue.png',
+            src: 'BanderoleFuzue.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: './public/BanderoleFuzue.png',
+            src: 'BanderoleFuzue.png',
             sizes: '512x512',
             type: 'image/png',
           },
