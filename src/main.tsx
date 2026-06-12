521d045785bf0e149e160a9d38883b907578b5ae
LRonnet <leo.ronnet@gmail.com>
2026-06-12 16:14:32 +0200

    JWA auto-update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'
+import { registerSW } from 'virtual:pwa-register'
 import { Provider } from 'react-redux'
 import {store} from './store/store'
-import './index.css'
+
+const updateSW = registerSW({
+  immediate: true,
+
+  onNeedRefresh() {
+    const ok = confirm("Nouvelle version disponible. Mettre à jour ?");
+    if (ok) updateSW(true)
+  },
+})
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
