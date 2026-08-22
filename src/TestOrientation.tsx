b574f3ae3c7432d165b329a569c3cb993134cd52
LRonnet <leo.ronnet@gmail.com>
2026-08-22 03:24:03 +0200

    orientation test

---
+++ src/TestOrientation.tsx
@@ -0,0 +1,7 @@
+function TestOrientation() {
+    return (
+        <div className="h-1/2 w-1/2 fixed inset-0 portrait:bg-red-500 landscape:bg-green-500 flex items-center justify-center text-white text-2xl">TestOrientation</div>
+    )
+}
+
+export default TestOrientation
